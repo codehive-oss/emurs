500f81f900fbbf78efbd1401c8c86e62467f9eaa
Eugene Matsumura <eugene.bb1@gmail.com>
2025-10-22 01:01:27 +0900

    refactor: move test roms to separate directory

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), anyhow::Error> {
     println!("Starting Emulator!");
 
     let mut ram = RAM::new();
-    ram.load("./6502_65C02_functional_tests/bin_files/6502_functional_test.bin")?;
+    ram.load("./vendor/6502_65C02_functional_tests/bin_files/6502_functional_test.bin")?;
 
     let mut cpu = CPU::new(ram, 1 << 16);
     cpu.run();
